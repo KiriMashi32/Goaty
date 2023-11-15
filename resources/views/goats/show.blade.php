113e1494fcf99289a69b38c09db2bba110c196b4
Kiri <46458337+KiriMashi32@users.noreply.github.com>
2023-11-15 08:32:18 +0100

    .

--- resources/views/goats/show.blade.php
+++ resources/views/goats/show.blade.php
@@ -2,10 +2,16 @@
 @section('main')
 <body>
     <h1>Just one goat</h1>
+    <a href='/goats/{{$goat->id}}/edit'>Modifier {{$goat->name}} </a>
     <ul>
         <li>{{$goat->id}}</li>
+        <li>{{$goat->sex}}</li>
         <li>{{$goat->name}}</li>
+        <li>{{$goat->birthday}}</li>
+        <li>{{$goat->color}}</li>
         <li>{{$goat->price}}</li>
     </ul>
+    <a href="/goats">All the goats</a>
+
 </body>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,81 +1,18 @@
 <?php
 
+use App\Http\Controllers\GoatsController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Goat;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+Route::get('/goats', [GoatsController::class, 'index']);
+Route::get('/goats/create', [GoatsController::class, 'create']);
+Route::get('/goats/{id}/edit', [GoatsController::class, 'edit']);
+Route::patch('/goats/{id}', [GoatsController::class, 'update']);
+Route::delete('/goats/{id}', [GoatsController::class, 'destroy']);
+Route::get('/goats/{id}', [GoatsController::class, 'show']);
+Route::post('/goats', [GoatsController::class, 'store']);
 
-
-//Index : voir tout les goats
-Route::get('/goats', function() {
-    // $goats = [
-    //     ['id' => 0, 'nom' => 'Boby', 'prix' => 50],
-    //     ['id' => 1, 'nom' => 'Bobo', 'prix' => 150],
-    //     ['id' => 2, 'nom' => 'Boba', 'prix' => 250],
-    //     ['id' => 3, 'nom' => 'Bobu', 'prix' => 350],
-    // ];
-    $goats = Goat::all();
-    
-
-    return view('goats.index', compact('goats')); 
-
-});
-
-
-
-Route::get('/goats/create', function() {
-
-    return view('goats.create');
-
-});
-    
-
-//Show : afficher un seul goat
-Route::get('/goats/{id}', function($id){
-    $goat = Goat::find($id);
-
-    // $goats = [
-    //     ['id' => 0, 'nom' => 'Boby', 'prix' => 50],
-    //     ['id' => 1, 'nom' => 'Bobo', 'prix' => 150],
-    //     ['id' => 2, 'nom' => 'Boba', 'prix' => 250],
-    //     ['id' => 3, 'nom' => 'Bobu', 'prix' => 350],
-    // ];
-        
-    // $goat = $goat->id;
-    // dd($id);
-    return view('goats.show', compact('goat'));
-});
-
-
-Route::post('/goats', function() {
-    $g = new Goat();
-    $g->sex = request()->sex == 'on' ? true : false;
-    $g->name = request()->name;
-    $g->price = request()->price;
-    $g->color = request()->color;
-    $g->birthday = request()->birthday;
-    $g->save();
-    return redirect('/goats/'.$g->id);
-
-
-});
-
-
-
-
-Route::get('/', function() {
+Route::get('/', function () {
     return redirect('/goats');
 });
-
-//Form : afficher un formulaire
-
